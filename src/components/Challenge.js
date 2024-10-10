56d209c6a7c7488f67bccdccc88ad44a0719ca24
romaingaget <romaingaget@gmail.com>
2024-10-10 17:47:03 +0100

    timer logic

--- src/components/Challenge.js
+++ src/components/Challenge.js
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import '../styles/Challenge.css';
+import { useRef } from 'react';
 
-export default function Challenge({title, targetTime}) {
+export default function Challenge({ title, targetTime }) {
+    const time = useRef()
+    const [timerStarted, setTimerStarted] = useState(false)
+    const [timerExpired, setTimerExpired] = useState(false)
+
+    const handleStart = (time) => {
+
+        time.current = setTimeout(() => {
+            setTimerExpired(true)
+        }, time * 1000)
+
+        setTimerStarted(true)
+    }
+
+    const handleStop = () => {
+        clearTimeout(time.current)
+    }
     return (
         <div className="challengeWrapper">
             <h2>{title}</h2>
             <p className="challenge-time">
                 {targetTime} seconds
             </p>
-            <button>Start Challenge</button>
+            <button onClick={timerStarted ? handleStop : handleStart}>{timerStarted ? 'Stop Challenge' : 'Start Challenge'}</button>
         </div>
     )
 }
